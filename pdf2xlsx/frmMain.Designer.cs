761ff02df39d59de4eb503e61cdb2bfa044fe4f8
Faruk Çakal <fcakal07@gmail.com>
2023-06-19 22:07:04 +0300

    Add project files.

---
+++ pdf2xlsx/frmMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace pdf2xlsx
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // selectFile
+            // 
+            this.selectFile.FileName = "selectFile";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(183, 43);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "PDF Seç";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 65);
+            this.Controls.Add(this.btnSelectFile);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "pdf2Excel by Faruk Çakal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog selectFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+    }
+}
+
---
+++ pdf2xlsx/frmMain.cs
@@ -0,0 +1,59 @@
+﻿using Aspose.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pdf2xlsx
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            selectFile.Filter = "PDF Dosyaları (*.pdf)|*.pdf";
+            DialogResult resultOpenFile = selectFile.ShowDialog();
+
+            if(resultOpenFile == DialogResult.OK)
+            {
+                Document pdfDocument = new Document(selectFile.FileName);
+
+
+                ExcelSaveOptions excelSave = new ExcelSaveOptions()
+                {
+                    MinimizeTheNumberOfWorksheets = true,
+                    Format = ExcelSaveOptions.ExcelFormat.XLSX,
+                    
+                };
+
+                saveFile.Filter = "Excel Dosyaları (*.xlsx)|*.xlsx*";
+                DialogResult resultSaveFile = saveFile.ShowDialog();
+
+                if(resultSaveFile == DialogResult.OK)
+                {
+
+                    pdfDocument.Save(saveFile.FileName + ".xlsx", excelSave);
+                    //document.Save(saveFile.FileName + ".xlsx", SaveFormat.Excel, excelSaveOptions);
+                    MessageBox.Show("Başarıyla dönüştürüldü.");
+                }
+                else
+                {
+                    MessageBox.Show("İşlem kullanıcı tarafından iptal edildi.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("İşlem kullanıcı tarafından iptal edildi.");
+            }
+        }
+    }
+}
